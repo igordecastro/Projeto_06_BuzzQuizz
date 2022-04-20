71bfbbb523e9fcaf7f981f94958efa686d09c58f
Igor <igordecastro07@gmail.com>
2022-04-20 20:43:43 -0300

    botao de criação

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,7 @@
+/* FUNÇÃO DO BOTÃO QUE CRIA QUIZZES
+    ao clicar muda a página para de criação */
+function criarQuizz() {
+    botao = document.querySelector(".nenhum-quizz");
+    botao.classList.add("escondido");
+    //depois de criar a página de criação fazer com que ela apareça nessa função
+}
